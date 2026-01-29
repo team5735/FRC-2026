babd2ed1e18db265f07da816140354e489c33a15
btho7793 <77469772+btho7793@users.noreply.github.com>
2026-01-28 20:14:04 -0500

    Remove old & unused constants

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -3,19 +3,6 @@ package frc.robot.constants;
 import edu.wpi.first.math.geometry.Rotation2d;
 
 public class Constants {
-    public static final int ALGAE_FALCON_ID = 57;
-
-    public static final int CORAL_MOTOR_TOP_ID = 26;
-    public static final int CORAL_MOTOR_BOTTOM_ID = 27;
-    public static final int CORAL_EJECTOR_ID = 25;
-
-    public static final int ELEVATOR_KRAKEN_RIGHT_ID = 24;
-    public static final int ELEVATOR_KRAKEN_LEFT_ID = 23;
-
-    public static final int FEEDER_FALCON_ID = 22;
-
-    public static final int INTAKE_BEAM_PIN = 0;
-
     public static final int DRIVE_CONTROLLER_PORT = 0;
     public static final int SUBSYSTEM_CONTROLLER_PORT = 1;
 
@@ -30,6 +17,4 @@ public class Constants {
     public static final double PID_P = 6;
     public static final double PID_I = 2;
     public static final double PID_D = 0;
-
-    public static final int CANDLE_ID = 33;
 }
