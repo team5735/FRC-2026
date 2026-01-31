ff0e1cf7e8f029d35ab5f109d9d7b1658c04e62c
btho7793 <77469772+btho7793@users.noreply.github.com>
2026-01-31 17:36:52 -0500

    silence joystick disconnected warning

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -10,6 +10,7 @@ import com.ctre.phoenix6.swerve.SwerveRequest;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.units.measure.Angle;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -44,6 +45,7 @@ public class RobotContainer {
     }
 
     public RobotContainer() {
+        DriverStation.silenceJoystickConnectionWarning(true);
         configureBindings();
     }
 
