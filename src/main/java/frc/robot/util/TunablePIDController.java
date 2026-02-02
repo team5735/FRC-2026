d42080c450c51ace984f104c6534a945c013fd44
Sebastian Gracia <sebastian.r.gracia@gmail.com>
2026-02-02 17:31:52 -0500

    rewrite TunablePIDController to use NTable and redocument

--- src/main/java/frc/robot/util/TunablePIDController.java
+++ src/main/java/frc/robot/util/TunablePIDController.java
@@ -4,60 +4,82 @@ import edu.wpi.first.math.controller.PIDController;
 import frc.robot.constants.Constants;
 
 public class TunablePIDController {
-    /** PID controller. */
+    /** The wrapped PID controller. */
     protected PIDController controller;
 
-    protected TunableNumber p, i, d;
-
+    /** The table used to manage PID values. */
     private NTable table;
 
-    /**
-     * Creates a new PIDCommand, which controls the given output with a
-     * PIDController.
-     *
-     * @param controller        the controller that controls the output.
-     * @param measurementSource the measurement of the process variable
-     * @param setpointSource    the controller's setpoint
-     * @param useOutput         the controller's output
-     * @param requirements      the subsystems required by this command
-     */
-    public TunablePIDController(
-            String name) {
-        this(name, Constants.PID_P, Constants.PID_I, Constants.PID_D);
+    /** Creates a new PID controller with the default values in /pid/{name}. */
+    public TunablePIDController(String name) {
+        this(NTable.root("pid"), name, Constants.PID_P, Constants.PID_I, Constants.PID_D);
     }
 
+    /** Creates a new PID controller with the specified values in /pid/{name}. */
     public TunablePIDController(String name, double _p, double _i, double _d) {
-        p = new TunableNumber("tunable_pid_commands", name + "_p", _p);
-        i = new TunableNumber("tunable_pid_commands", name + "_i", _i);
-        d = new TunableNumber("tunable_pid_commands", name + "_d", _d);
-
-        table = NTable.root("pid").sub(name);
+        this(NTable.root("pid"), name, _p, _i, _d);
     }
 
+    /** Creates a new PID controller with the default values in {table}/{name}. */
+    public TunablePIDController(NTable table, String name) {
+        this(table, name, Constants.PID_P, Constants.PID_I, Constants.PID_D);
+    }
+
+    /** Creates a new PID controller with the specified values in {table}/{name}. */
+    public TunablePIDController(NTable table, String name, double _p, double _i, double _d) {
+        this.table = table.sub(name);
+        this.table.set("kP", _p);
+        this.table.set("kI", _i);
+        this.table.set("kD", _d);
+        this.table.makePersistent("kP", "kI", "kD");
+    }
+
+    /**
+     * Set up this PID controller.
+     *
+     * <p>
+     * This retrieves the PID constants from the table created during construction.
+     * It also telemeterizes the setpoint and sets the controller to have the
+     * specified setpoint and the default tolerance.
+     *
+     * @param setpoint
+     */
     public void setup(double setpoint) {
         setup(setpoint, Constants.TOLERANCE);
     }
 
+    /**
+     * Set up this PID controller.
+     *
+     * <p>
+     * This retrieves the PID constants from the table created during construction.
+     * It also telemeterizes the setpoint and sets the controller to have the
+     * specified setpoint and tolerance.
+     *
+     * @param setpoint
+     * @param tolerance
+     */
     public void setup(double setpoint, double tolerance) {
-        controller = new PIDController(p.get(), i.get(), d.get());
-        table.set("p", p.get());
-        table.set("i", i.get());
-        table.set("d", d.get());
+        controller = new PIDController(
+                table.getDouble("kP"),
+                table.getDouble("kI"),
+                table.getDouble("kD"));
 
         controller.setSetpoint(setpoint);
         table.set("setpoint", setpoint);
         controller.setTolerance(tolerance);
     }
 
+    /** Resets the PID controller. */
     public void reset() {
         controller.reset();
     }
 
     /**
-     * Runs the PID calculation.
+     * Returns the next output of the PID controller.
      * 
      * <p>
-     * Outputs 0 if the controller is at the setpoint.
+     * Also telemeterizes the given and PID controller output.
      * 
      * @param measurement
      * @return The controller output, or zero if atSetpoint.
@@ -69,18 +91,12 @@ public class TunablePIDController {
         return value;
     }
 
-    /**
-     * @return Whether the PIDController is within tolerance of the setpoint.
-     */
+    /** {@return whether this PIDController is within tolerance of the setpoint} */
     public boolean atSetpoint() {
         return this.controller.atSetpoint();
     }
 
-    /**
-     * Returns the PIDController used by the command.
-     *
-     * @return The PIDController
-     */
+    /** {@return the wrapped PIDController} */
     public PIDController getController() {
         return controller;
     }
