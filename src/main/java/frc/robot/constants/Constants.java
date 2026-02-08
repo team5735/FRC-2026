96f5739a12fe5be85f0fead5cb1eeac1b2cab131
Sebastian Gracia <sebastian.r.gracia@gmail.com>
2026-02-08 15:05:03 -0500

    add TunableProfiledPIDController

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -25,4 +25,11 @@ public class Constants {
     public static final double PID_P = 6;
     public static final double PID_I = 2;
     public static final double PID_D = 0;
+
+    public static final double PROFILED_PID_P = 2;
+    public static final double PROFILED_PID_I = 0;
+    public static final double PROFILED_PID_D = 0;
+
+    public static final double MAX_VELOCITY = 1;
+    public static final double MAX_ACCELERATION = 1;
 }
