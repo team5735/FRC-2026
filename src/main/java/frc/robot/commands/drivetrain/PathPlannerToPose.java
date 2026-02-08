44ba528660be9170f1a09718b3d58cb61b1a984c
Sebastian Gracia <sebastian.r.gracia@gmail.com>
2026-02-08 14:04:13 -0500

    don't use deprecated Command.schedule()

--- src/main/java/frc/robot/commands/drivetrain/PathPlannerToPose.java
+++ src/main/java/frc/robot/commands/drivetrain/PathPlannerToPose.java
@@ -13,6 +13,7 @@ import com.pathplanner.lib.path.Waypoint;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.subsystems.DrivetrainSubsystem;
 
 public class PathPlannerToPose extends Command {
@@ -43,7 +44,7 @@ public class PathPlannerToPose extends Command {
         path.preventFlipping = true;
 
         // replace this command with the pathfindThenFollowPath command
-        AutoBuilder.pathfindThenFollowPath(path, constraints).schedule();
+        CommandScheduler.getInstance().schedule(AutoBuilder.pathfindThenFollowPath(path, constraints));
         this.cancel();
     }
 
