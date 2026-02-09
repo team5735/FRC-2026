bc300ec975ad8ec342e33762f3745f87a4b2d9d9
JacobyIA <jacoby.adwin@gmail.com>
2026-02-09 07:40:01 -0500

    WIP turret mechanism addition

--- src/main/java/frc/robot/constants/TurretConstants.java
+++ src/main/java/frc/robot/constants/TurretConstants.java
@@ -1,5 +1,16 @@
 package frc.robot.constants;
 
+import static edu.wpi.first.units.Units.Rotations;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.units.measure.Angle;
+
 public class TurretConstants {
     public static final int TESTING_VOLTS = 1;
+    public static final double GEAR_REDUCTION = 1; // initial gear ____T, secondary gear 200T
+    public static final PIDController PID = new PIDController(0, 0, 0);
+    public static final SimpleMotorFeedforward FF = new SimpleMotorFeedforward(0, 0, 0);
+    public static final Angle LOWER_LIMIT = Rotations.of(0);
+    public static final Angle UPPER_LIMIT = Rotations.of(1)
 }
--- src/main/java/frc/robot/subsystems/TurretSubsystem.java
+++ src/main/java/frc/robot/subsystems/TurretSubsystem.java
@@ -1,27 +1,58 @@
 package frc.robot.subsystems;
 
+import static edu.wpi.first.units.Units.Rotation;
+import static edu.wpi.first.units.Units.Rotations;
+
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import frc.robot.constants.Constants;
-import frc.robot.constants.TurretConstants;
+import static frc.robot.constants.TurretConstants.*;
+import yams.mechanisms.config.PivotConfig;
+import yams.mechanisms.positional.Pivot;
+import yams.motorcontrollers.SmartMotorController;
+import yams.motorcontrollers.SmartMotorControllerConfig;
+import yams.motorcontrollers.SmartMotorControllerConfig.ControlMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.MotorMode;
+import yams.motorcontrollers.SmartMotorControllerConfig.TelemetryVerbosity;
+import yams.motorcontrollers.remote.TalonFXWrapper;
+import edu.wpi.first.math.controller.SimpleMotorFeedforward;
+import edu.wpi.first.math.system.plant.DCMotor;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class TurretSubsystem extends SubsystemBase {
     private final TalonFX kraken = new TalonFX(Constants.TURRET_MOTOR_ID);
 
+    private final SmartMotorControllerConfig krakenControllerConfig = new SmartMotorControllerConfig(this)
+        .withControlMode(ControlMode.CLOSED_LOOP)
+        .withClosedLoopController(PID)
+        .withFeedforward(FF)
+        .withTelemetry("turret_motor", TelemetryVerbosity.HIGH)
+        .withGearing(GEAR_REDUCTION)
+        .withIdleMode(MotorMode.BRAKE)
+        .withMotorInverted(false);
+
+    private final SmartMotorController krakenController = new TalonFXWrapper(kraken, DCMotor.getKrakenX44(1), krakenControllerConfig);
+    
+    private final PivotConfig mechanismConfig = new PivotConfig()
+        .withHardLimit(LOWER_LIMIT, UPPER_LIMIT)
+        .withSmartMotorController(krakenController)
+        .withTelemetry("turret", TelemetryVerbosity.HIGH);
+        
+
+    private final Pivot turretMechanism = new Pivot(null)
+
     public TurretSubsystem() {
-        kraken.getConfigurator().apply(new MotorOutputConfigs().withNeutralMode(NeutralModeValue.Brake));
     }
 
     private void testForward() {
-        kraken.setVoltage(TurretConstants.TESTING_VOLTS);
+        kraken.setVoltage(TESTING_VOLTS);
     }
 
     private void testReverse() {
-        kraken.setVoltage(-TurretConstants.TESTING_VOLTS);
+        kraken.setVoltage(-TESTING_VOLTS);
     }
 
     private void stop() {
