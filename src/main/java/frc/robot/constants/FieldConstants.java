d83f7f3dec4d2bfa75a863abd5d563c45f835204
Joshua Migdal <jmigdal@amazon.com>
2026-02-10 12:48:36 -0500

    added tower constants and climber waypoints. unit tests
    also added redElement to transform a Pose2d

--- src/main/java/frc/robot/constants/FieldConstants.java
+++ src/main/java/frc/robot/constants/FieldConstants.java
@@ -5,6 +5,9 @@ import static edu.wpi.first.units.Units.Inches;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.units.measure.Distance;
@@ -19,13 +22,24 @@ public class FieldConstants {
     }
 
     /**
-     * Transforms a blue-alliance {@link Translation2d} into a red-alliance
-     * {@link Translation2d}
+     * Gets the red-alliance equivalent of the given blue field element.
+     * The returned dimensions are STILL in wpiBlue coords
      * 
-     * @return red-alliance {@link Translation2d}
+     * @return red-alliance field element{@link Translation2d}
      */
     public static Translation2d redElement(Translation2d blueElement) {
         return FAR_CORNER.minus(blueElement);
+    }
+
+    /**
+     * Gets the red-alliance equivalent of the given blue field element.
+     * The returned dimensions are STILL in wpiBlue coords
+     * 
+     * @return red-alliance field element{@link Translation2d}
+     */
+    public static Pose2d redElement(Pose2d blueElement) {
+        return new Pose2d(redElement(blueElement.getTranslation()),
+                          blueElement.getRotation().plus(Rotation2d.fromDegrees(180)));
     }
 
     /**
@@ -43,7 +57,7 @@ public class FieldConstants {
     // into wpilib
     // The terms length and width (and height and depth) are ambiguous.
     // Instead of using those, we say the long side "length" of the field is
-    // length_x and the short side "width" of the field if length_y.
+    // length_x and the short side "width" of the field is length_y.
     // All of our dimensions will be length_x, length_y, and length_z in wpiblue
     // field coordinates meaning downfield is in the x- direction, across the short
     // side of the field is y, and up in the air is z.
@@ -64,21 +78,68 @@ public class FieldConstants {
     public static final Translation2d BLUE_HUB_CENTER = new Translation2d(inch(181.56), inch(158.32));
     public static final Translation2d BLUE_OUTPOST_CENTER = new Translation2d(inch(0), inch(25.62));
 
+    //////////////////////
+    // TRENCH CONSTANTS
     public static final Translation2d BLUE_TRENCH_RIGHT_CENTER = new Translation2d(inch(181.56), inch(24.97));
     public static final Translation2d BLUE_TRENCH_LEFT_CENTER = new Translation2d(inch(181.56),
             inch(FIELD_LENGTH_Y.in(Inches) - 24.97));
     public static final Translation3d TRENCH_DIMENSION = new Translation3d(
             inch(0), // TODO: incorrect
-            inch(2 * 24.97), // opening extent along y-axis
-            inch(22.25)); // opening extent along z-axis
+            inch(2 * 24.97), // how wide it is to fit a bot through
+            inch(22.25)); // height to the ceiling of the tunnel
 
+    //////////////////////
+    // RAMP CONSTANTS
     public static final Translation2d BLUE_RAMP_RIGHT_CENTER = new Translation2d(inch(181.56),
             inch(24.97 * 2 + 12 + 73 / 2.0));
     public static final Translation2d BLUE_RAMP_LEFT_CENTER = new Translation2d(inch(181.56),
             inch(FIELD_LENGTH_Y.in(Inches) - (24.97 * 2 + 12 + 73 / 2.0)));
 
     public static final Translation3d RAMP_DIMENSION = new Translation3d(
-            inch(44.4), // extent across the x-axis
-            inch(73), // extent across the y-axis
+            inch(44.4), // the up-then-down total length of the ramp
+            inch(73), // how wide it is to fit a bot through
             inch(6.513)); // ramp starts at 0 height and goes to this height in the middle
+
+    //////////////////////
+    // TOWER CONSTANTS
+    // Field location of the center-front of the climbing tower
+    // This is to the front of the uprights, which are a little further ahead than the rungs,
+    // and a little further back than the climbing structure floor (see pdf for details)
+    public static final Translation2d BLUE_TOWER_FRONT_CENTER = new Translation2d(inch(43.8),inch(146.86));
+    public static final Translation3d TOWER_DIMENSION = new Translation3d(
+            inch(43.8), // how much does tower stick out onto field
+            inch(47.0), // from outside end-to-end of the rungs
+            inch(27)); // height to center of first rung
+    public static final Distance TOWER_RUNG_OD = inch(1.66); // outer diameter of each rung of the tower
+    public static final int BLUE_TOWER_TAG_1 = 31; // april tag IDs associated with the towers
+    public static final int BLUE_TOWER_TAG_2 = 32;
+    public static final int RED_TOWER_TAG_1 = 15;
+    public static final int RED_TOWER_TAG_2 = 16;
+
+
+    ///////////////////////////////
+    // CLIMBING AUTO WAYPOINTS
+    // this is the height we need to get our hook over to latch onto the lowest rung
+    public static final Distance CLIMBER_HOOK_MIN_Z = TOWER_DIMENSION.getMeasureZ().plus(TOWER_RUNG_OD.div(2.0)).plus(inch(0.25));
+
+    // right in front of tower, 1' away from it
+    public static final Pose2d CLIMBER_BLUE_LOCALIZATION_POSE = new Pose2d(
+        BLUE_TOWER_FRONT_CENTER.plus(new Translation2d(inch(30+12),inch(0))),
+        Rotation2d.fromDegrees(180));
+
+    // center bot on rung, position 1' to left of tower
+    public static final Pose2d CLIMBER_BLUE_LEFT_CLIMB_ALIGN_POSE = new Pose2d(
+        BLUE_TOWER_FRONT_CENTER.plus(new Translation2d(inch(-3.51/2.0), // half the thickness of the uprights
+                                                       TOWER_DIMENSION.getMeasureY().div(-2.0).minus(inch(30/2.0+12)))),
+        Rotation2d.fromDegrees(180));
+
+    // where we should be to actually do the climb
+    //   todo: right now this is to center of bot, need to add offset to hook
+    public static final Pose2d CLIMBER_BLUE_LEFT_CLIMB_POSE = new Pose2d(
+        CLIMBER_BLUE_LEFT_CLIMB_ALIGN_POSE.getX(),
+        BLUE_TOWER_FRONT_CENTER.getY()-TOWER_DIMENSION.getY()/2.0+inch(5.875).in(Meters)/2.0, // 5.875"=rung extent
+        CLIMBER_BLUE_LEFT_CLIMB_ALIGN_POSE.getRotation()
+    );
+    
 }
+
--- src/test/java/frc/constants/FieldMapTest.java
+++ src/test/java/frc/constants/FieldMapTest.java
@@ -1,12 +1,22 @@
 package frc.constants;
 
 import org.junit.jupiter.api.Test;
+import static edu.wpi.first.units.Units.Inches;
 
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import frc.robot.constants.FieldConstants;
 
 public class FieldMapTest {
+    public static String pretty(Translation2d d){
+        return String.format("(%.2f\", %.2f\")",d.getMeasureX().in(Inches), d.getMeasureY().in(Inches));
+    }
+    public static String pretty(Pose2d p){
+        return String.format("%s %.1f°",pretty(p.getTranslation()), p.getRotation().getDegrees());
+    }
+
     @Test
     void fieldMapTest() throws Exception {
         System.out.printf("Field Center: %s\n",FieldConstants.FIELD_CENTER.toString());
@@ -40,6 +50,18 @@ public class FieldMapTest {
         }
         System.out.printf("Field length: %f\n",atf.getFieldLength());
         System.out.printf("Field width: %f\n",atf.getFieldWidth());
+
+        System.out.printf("Blue Tower Front Center: %s\n", pretty(FieldConstants.BLUE_TOWER_FRONT_CENTER));
+        System.out.printf("  Climber Hook Min Z: %.2f\"\n", FieldConstants.CLIMBER_HOOK_MIN_Z.in(Inches));
+        System.out.printf("  Climber Localization Pose: %s\n", pretty(FieldConstants.CLIMBER_BLUE_LOCALIZATION_POSE));
+        System.out.printf("  Climber Left Align Pose: %s\n", pretty(FieldConstants.CLIMBER_BLUE_LEFT_CLIMB_ALIGN_POSE));
+        System.out.printf("  Climber Left Climb Pose: %s\n", pretty(FieldConstants.CLIMBER_BLUE_LEFT_CLIMB_POSE));
+
+        System.out.printf("Red Tower Front Center: %s\n", pretty(FieldConstants.redElement(FieldConstants.BLUE_TOWER_FRONT_CENTER)));
+        System.out.printf("  Climber Hook Min Z: %.2f\"\n", FieldConstants.CLIMBER_HOOK_MIN_Z.in(Inches));
+        System.out.printf("  Climber Localization Pose: %s\n", pretty(FieldConstants.redElement(FieldConstants.CLIMBER_BLUE_LOCALIZATION_POSE)));
+        System.out.printf("  Climber Left Align Pose: %s\n", pretty(FieldConstants.redElement(FieldConstants.CLIMBER_BLUE_LEFT_CLIMB_ALIGN_POSE)));
+        System.out.printf("  Climber Left Climb Pose: %s\n", pretty(FieldConstants.redElement(FieldConstants.CLIMBER_BLUE_LEFT_CLIMB_POSE)));
 
     }
 }
