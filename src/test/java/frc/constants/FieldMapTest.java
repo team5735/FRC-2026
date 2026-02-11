f8555354e18326ee90a8ffd51c3fdab461e1d79f
Sebastian Gracia <sebastian.r.gracia@gmail.com>
2026-02-11 14:25:07 -0500

    reformat FieldMapTest completely (use println instead of printf)

--- src/test/java/frc/constants/FieldMapTest.java
+++ src/test/java/frc/constants/FieldMapTest.java
@@ -1,67 +1,98 @@
 package frc.constants;
 
-import org.junit.jupiter.api.Test;
+import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.Inches;
 
+import org.junit.jupiter.api.Test;
+
+import edu.wpi.first.apriltag.AprilTag;
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Pose3d;
+import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
 import frc.robot.constants.FieldConstants;
 
 public class FieldMapTest {
-    public static String pretty(Translation2d d){
-        return String.format("(%.2f\", %.2f\")",d.getMeasureX().in(Inches), d.getMeasureY().in(Inches));
+    public static String pretty(Translation2d d) {
+        return String.format("(%.2f\", %.2f\")", d.getMeasureX().in(Inches), d.getMeasureY().in(Inches));
     }
-    public static String pretty(Pose2d p){
-        return String.format("%s %.1f°",pretty(p.getTranslation()), p.getRotation().getDegrees());
+
+    public static String pretty(Pose2d p) {
+        return String.format("%s %.1f°", pretty(p.getTranslation()), p.getRotation().getDegrees());
+    }
+
+    private static String pretty(Translation3d d) {
+        return String.format("(%.2f\", %.2f\", %.2f\")", d.getMeasureX().in(Inches), d.getMeasureY().in(Inches),
+                d.getMeasureZ().in(Inches));
+    }
+
+    private static String pretty(Rotation3d r) {
+        return String.format("(%.2f°, %.2f°, %.2f°)",
+                r.getMeasureX().in(Degrees), r.getMeasureY().in(Degrees), r.getMeasureZ().in(Degrees));
+    }
+
+    private static String pretty(Pose3d p) {
+        return String.format("%s %.1f°", pretty(p.getTranslation()), pretty(p.getRotation()));
+    }
+
+    private static Translation2d red(Translation2d in) {
+        return FieldConstants.redElement(in);
+    }
+
+    private static Pose2d red(Pose2d in) {
+        return FieldConstants.redElement(in);
     }
 
     @Test
     void fieldMapTest() throws Exception {
-        System.out.printf("Field Center: %s\n",FieldConstants.FIELD_CENTER.toString());
-        System.out.printf("\n");
+        System.out.println("Field Center: " + FieldConstants.FIELD_CENTER);
+        System.out.println();
 
-        System.out.printf("Blue hub: %s\n",FieldConstants.BLUE_HUB_CENTER.toString());
-        System.out.printf("Red hub: %s\n",FieldConstants.redElement(FieldConstants.BLUE_HUB_CENTER).toString());
-        System.out.printf("\n");
+        System.out.println("Blue hub: " + FieldConstants.BLUE_HUB_CENTER);
+        System.out.println("Red hub: " + red(FieldConstants.BLUE_HUB_CENTER));
+        System.out.println();
 
-        System.out.printf("Blue outpost: %s\n",FieldConstants.BLUE_OUTPOST_CENTER.toString());
-        System.out.printf("Red outpost: %s\n",FieldConstants.redElement(FieldConstants.BLUE_OUTPOST_CENTER).toString());
-        System.out.printf("\n");
+        System.out.println("Blue outpost: " + FieldConstants.BLUE_OUTPOST_CENTER);
+        System.out.println("Red outpost: " + red(FieldConstants.BLUE_OUTPOST_CENTER));
+        System.out.println();
 
-        System.out.printf("Blue trench right: %s\n",FieldConstants.BLUE_TRENCH_RIGHT_CENTER.toString());
-        System.out.printf("Red trench right: %s\n",FieldConstants.redElement(FieldConstants.BLUE_TRENCH_RIGHT_CENTER).toString());
+        System.out.println("Blue trench right: " + FieldConstants.BLUE_TRENCH_RIGHT_CENTER);
+        System.out.println("Red trench right: " + red(FieldConstants.BLUE_TRENCH_RIGHT_CENTER));
 
-        System.out.printf("Blue trench left: %s\n",FieldConstants.BLUE_TRENCH_LEFT_CENTER.toString());
-        System.out.printf("Red trench left: %s\n",FieldConstants.redElement(FieldConstants.BLUE_TRENCH_LEFT_CENTER).toString());
-        System.out.printf("\n");
+        System.out.println("Blue trench left: " + FieldConstants.BLUE_TRENCH_LEFT_CENTER);
+        System.out.println("Red trench left: " + red(FieldConstants.BLUE_TRENCH_LEFT_CENTER));
+        System.out.println();
 
-        System.out.printf("Blue ramp right: %s\n",FieldConstants.BLUE_RAMP_RIGHT_CENTER.toString());
-        System.out.printf("Red ramp right: %s\n",FieldConstants.redElement(FieldConstants.BLUE_RAMP_RIGHT_CENTER).toString());
+        System.out.println("Blue ramp right: " + FieldConstants.BLUE_RAMP_RIGHT_CENTER);
+        System.out.println("Red ramp right: " + red(FieldConstants.BLUE_RAMP_RIGHT_CENTER));
 
-        System.out.printf("Blue ramp left: %s\n",FieldConstants.BLUE_RAMP_LEFT_CENTER.toString());
-        System.out.printf("Red ramp left: %s\n",FieldConstants.redElement(FieldConstants.BLUE_RAMP_LEFT_CENTER).toString());
+        System.out.println("Blue ramp left: " + FieldConstants.BLUE_RAMP_LEFT_CENTER);
+        System.out.println("Red ramp left: " + red(FieldConstants.BLUE_RAMP_LEFT_CENTER));
 
         AprilTagFieldLayout atf = AprilTagFieldLayout.loadField(AprilTagFields.k2026RebuiltAndymark);
-        System.out.printf("april tags:\n");
-        for (var a:atf.getTags()){
-            System.out.printf("  %s\n",a.toString());
+        System.out.println("AprilTags:");
+        for (AprilTag tag : atf.getTags()) {
+            System.out.printf("  " + tag.ID + " = " + pretty(tag.pose));
         }
-        System.out.printf("Field length: %f\n",atf.getFieldLength());
-        System.out.printf("Field width: %f\n",atf.getFieldWidth());
 
-        System.out.printf("Blue Tower Front Center: %s\n", pretty(FieldConstants.BLUE_TOWER_FRONT_CENTER));
-        System.out.printf("  Climber Hook Min Z: %.2f\"\n", FieldConstants.CLIMBER_HOOK_MIN_Z.in(Inches));
-        System.out.printf("  Climber Localization Pose: %s\n", pretty(FieldConstants.CLIMBER_BLUE_LOCALIZATION_POSE));
-        System.out.printf("  Climber Left Align Pose: %s\n", pretty(FieldConstants.CLIMBER_BLUE_LEFT_CLIMB_ALIGN_POSE));
-        System.out.printf("  Climber Left Climb Pose: %s\n", pretty(FieldConstants.CLIMBER_BLUE_LEFT_CLIMB_POSE));
+        System.out.println("Field length: " + atf.getFieldLength());
+        System.out.println("Field width: " + atf.getFieldWidth());
 
-        System.out.printf("Red Tower Front Center: %s\n", pretty(FieldConstants.redElement(FieldConstants.BLUE_TOWER_FRONT_CENTER)));
-        System.out.printf("  Climber Hook Min Z: %.2f\"\n", FieldConstants.CLIMBER_HOOK_MIN_Z.in(Inches));
-        System.out.printf("  Climber Localization Pose: %s\n", pretty(FieldConstants.redElement(FieldConstants.CLIMBER_BLUE_LOCALIZATION_POSE)));
-        System.out.printf("  Climber Left Align Pose: %s\n", pretty(FieldConstants.redElement(FieldConstants.CLIMBER_BLUE_LEFT_CLIMB_ALIGN_POSE)));
-        System.out.printf("  Climber Left Climb Pose: %s\n", pretty(FieldConstants.redElement(FieldConstants.CLIMBER_BLUE_LEFT_CLIMB_POSE)));
+        System.out.println("Blue-alliance climber poses:");
+        System.out.println("  Blue tower front center: " + pretty(FieldConstants.BLUE_TOWER_FRONT_CENTER));
+        System.out.println("  Hook min Z: " + FieldConstants.CLIMBER_HOOK_MIN_Z.in(Inches) + "\"");
+        System.out.println("  Localization pose: " + pretty(FieldConstants.CLIMBER_BLUE_LOCALIZATION_POSE));
+        System.out.println("  Left align pose: " + pretty(FieldConstants.CLIMBER_BLUE_LEFT_CLIMB_ALIGN_POSE));
+        System.out.println("  Left climb pose: " + pretty(FieldConstants.CLIMBER_BLUE_LEFT_CLIMB_POSE));
 
+        System.out.println("Red-alliance climber poses:");
+        System.out.println("  Red Tower Front Center: " + pretty(red(FieldConstants.BLUE_TOWER_FRONT_CENTER)));
+        System.out.println("  Hook min Z: " + FieldConstants.CLIMBER_HOOK_MIN_Z.in(Inches) + "\"");
+        System.out.println("  Localization pose: " + pretty(red(FieldConstants.CLIMBER_BLUE_LOCALIZATION_POSE)));
+        System.out.println("  Left align pose: " + pretty(red(FieldConstants.CLIMBER_BLUE_LEFT_CLIMB_ALIGN_POSE)));
+        System.out.println("  Left climb pose: " + pretty(red(FieldConstants.CLIMBER_BLUE_LEFT_CLIMB_POSE)));
     }
 }
