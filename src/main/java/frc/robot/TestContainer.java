316cbb3bad32e4a7d828a52e1651a2496ab3c706
MathMan5 <Ryan.Blocker@outlook.com>
2026-02-18 14:43:52 -0500

    Added Test Container

--- src/main/java/frc/robot/TestContainer.java
+++ src/main/java/frc/robot/TestContainer.java
@@ -1,16 +1,18 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import frc.robot.commands.MoveCommand;
 import frc.robot.constants.Constants;
-import frc.robot.subsystems.SpinDexSubsystem;
+import frc.robot.subsystems.HoodSubsystem;
 
 public class TestContainer {
     private static final CommandXboxController driveController = new CommandXboxController(
             Constants.DRIVE_CONTROLLER_PORT);
 
-    private static final SpinDexSubsystem spindex = new SpinDexSubsystem();
+    private static final HoodSubsystem hood = new HoodSubsystem();
 
     public static void configureBindings() {
-        driveController.b().onTrue(spindex.getStart());
+        driveController.a().onTrue(new MoveCommand(hood, 0.6));
+        driveController.a().onFalse(new MoveCommand(hood, 0.4));
     }
 }
