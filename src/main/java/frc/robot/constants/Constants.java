a3f0aa442838c4befff9ca7ace31696df34b66ea
Sebastian Gracia <sebastian.r.gracia@gmail.com>
2026-02-18 17:14:56 -0500

    fix errors caused by merging in dev poorly

--- src/main/java/frc/robot/constants/Constants.java
+++ src/main/java/frc/robot/constants/Constants.java
@@ -1,19 +1,16 @@
 package frc.robot.constants;
 
-/*
- The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- constants. This class should not be used for any other purpose. All constants should be declared
- globally (i.e. public static). Do not put anything functional in this class.
+import edu.wpi.first.math.geometry.Rotation2d;
 
- It is advised to statically import this class (or one of its inner classes) wherever the
- constants are needed, to reduce verbosity.
-*/
-public final class Constants {
-    public static final int DRIVE_CONTROLLER_PORT = 0;
-    public static final int LAUNCHER_VOLTS = 4;
-    public static final int LAUNCHER_LEFT_KRAKEN_ID = 1;
-    public static final int LAUNCHER_RIGHT_KRAKEN_ID = 0;
-
+/**
+ * The Constants class provides a convenient place for teams to hold robot-wide
+ * numerical or boolean constants. This class should not be used for any other
+ * purpose. All constants should be declared globally (i.e. public static). Do
+ * not put anything functional in this class.
+ * 
+ * It is advised to statically import this class (or one of its inner classes)
+ * wherever the constants are needed, to reduce verbosity.
+ */
 public class Constants {
     public enum DrivetrainType {
         DEVBOT,
@@ -35,4 +32,7 @@ public class Constants {
 
     public static final int motor6 = 15;
     public static final int motor27 = 27;
+
+    public static final int LAUNCHER_LEFT_KRAKEN_ID = 1;
+    public static final int LAUNCHER_RIGHT_KRAKEN_ID = 0;
 }
--- src/main/java/frc/robot/constants/FuelLauncherConstants.java
+++ src/main/java/frc/robot/constants/FuelLauncherConstants.java
@@ -4,6 +4,4 @@ public class FuelLauncherConstants {
     public static final double RPM_TOLERANCE = 35; // 35 is 1% of expected RPM
 
     public static final int LAUNCHER_VOLTS = 4;
-    public static final int LAUNCHER_LEFT_KRAKEN_ID = 1;
-    public static final int LAUNCHER_RIGHT_KRAKEN_ID = 0;
 }
