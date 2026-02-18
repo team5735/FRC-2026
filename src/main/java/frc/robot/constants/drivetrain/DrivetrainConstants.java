4f0c6dee3a614c2c0c03e041b0727aefd57febed
Sebastian Gracia <sebastian.r.gracia@gmail.com>
2026-02-18 14:17:17 -0500

    Revert "Removed drivetrain code for testing with breadboard"
    This reverts commit 3de8823b0ad9c735e2d38e151eb0fc0ce4f522ad.

---
+++ src/main/java/frc/robot/constants/drivetrain/DrivetrainConstants.java
@@ -0,0 +1,57 @@
+package frc.robot.constants.drivetrain;
+
+import com.pathplanner.lib.config.PIDConstants;
+import com.pathplanner.lib.config.RobotConfig;
+import com.pathplanner.lib.path.PathConstraints;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.units.measure.AngularVelocity;
+import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.LinearVelocity;
+import edu.wpi.first.units.measure.Mass;
+
+public interface DrivetrainConstants {
+    public PIDConstants getAutoPosConstants();
+
+    public PIDConstants getAutoRotConstants();
+
+    public double getRotKs();
+
+    public double getRotKv();
+
+    public double getRotKa();
+
+    public double getOpenDriveKv();
+
+    public double getOpenDriveKa();
+
+    public Distance getRobotTotalWidth();
+
+    public Distance getRobotTotalLength();
+
+    public Distance getPigeonToRobotFront();
+
+    public PathConstraints getPathFollowConstraints();
+
+    public LinearVelocity getDefaultSpeed();
+
+    public LinearVelocity getSlowSpeed();
+
+    public AngularVelocity getDefaultRotationalRate();
+
+    public AngularVelocity getSlowRotationalRate();
+
+    public Mass getRobotMass();
+
+    public Distance getDrivetrainWidth();
+
+    public double getRobotMoiKgxMxM();
+
+    public double getCoefficientOfFriction();
+
+    public RobotConfig getConfig();
+
+    public Translation2d getPigeonToCenterOfRotation();
+
+    public Distance getBumperWidth();
+}
