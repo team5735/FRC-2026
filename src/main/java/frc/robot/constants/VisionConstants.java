e70c204f93bf511ba13045c6a62b02325361a577
Sebastian Gracia <sebastian.r.gracia@gmail.com>
2026-02-20 16:28:42 -0500

    oops, misarranged the constants

--- src/main/java/frc/robot/constants/VisionConstants.java
+++ src/main/java/frc/robot/constants/VisionConstants.java
@@ -17,12 +17,11 @@ public class VisionConstants {
 
     public static final Distance        TOLERATED_HEIGHT          = Centimeters.of(25);
     public static final AngularVelocity TOLERATED_ROTATIONAL_RATE = DegreesPerSecond.of(10);
-    public static final AngularVelocity MAX_ROTATION_SPEED        = DegreesPerSecond.of(1);
 
     public static final Time            MIN_RESET_DELAY          = Seconds.of(5);
     public static final Distance        NEAR_ENOUGH_TO_RESET     = Meters.of(1.5);
     public static final Angle           MAX_YAW_STDDEV_FOR_RESET = Degrees.of(5);
-    public static final AngularVelocity MAX_FOR_RESET            = DegreesPerSecond.of(0.5);
+    public static final AngularVelocity MAX_ROTATION_SPEED       = DegreesPerSecond.of(1);
 
     public static final double SINGLE_TAG_MAX_AMBIGUITY = 0.2;
     public static final double MULTI_TAG_MAX_AMBIGUITY  = 0.5;
