fce01067134a866e6401748077c8cc59f4bce4ec
Sebastian Gracia <sebastian.r.gracia@gmail.com>
2026-02-20 14:49:15 -0500

    disable compile error in RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,12 +4,20 @@
 
 package frc.robot;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import com.ctre.phoenix6.swerve.SwerveRequest;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+import com.pathplanner.lib.commands.PathfindingCommand;
 
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.constants.Constants;
 import frc.robot.constants.drivetrain.CompbotTunerConstants;
@@ -46,6 +54,15 @@ public class RobotContainer {
     public static final VisionSubsystem vision = new VisionSubsystem(drivetrain);
 
     public RobotContainer() { // Sebastian bug
+        Map<String, Command> commandsForAuto = new HashMap<>();
+
+        NamedCommands.registerCommands(commandsForAuto);
+
+        autoChooser = AutoBuilder.buildAutoChooser();
+
+        SmartDashboard.putData("Choose an Auto", autoChooser);
+        CommandScheduler.getInstance().schedule(PathfindingCommand.warmupCommand());
+
         DriverStation.silenceJoystickConnectionWarning(true);
         configureBindings();
     }
