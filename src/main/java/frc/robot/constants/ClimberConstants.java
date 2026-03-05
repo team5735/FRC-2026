1126c64ae13f57a2cae9fa5b323172723ad35c3f
btho7793 <77469772+btho7793@users.noreply.github.com>
2026-03-04 20:17:15 -0500

    Constant updates

--- src/main/java/frc/robot/constants/ClimberConstants.java
+++ src/main/java/frc/robot/constants/ClimberConstants.java
@@ -1,9 +1,9 @@
 package frc.robot.constants;
 
 public class ClimberConstants {
-    public static final int TALON_ID = 23;
-    public static final double UP_VOLTS = 1.0;
-    public static final double DOWN_VOLTS = -1.0;
+    public static final int TALON_ID = 24;
+    public static final double UP_VOLTS = 2.0;
+    public static final double DOWN_VOLTS = -2.0;
     public static final int UPPER_LIMIT_PIN = 0;
     public static final int LOWER_LIMIT_PIN = 1;
 }
