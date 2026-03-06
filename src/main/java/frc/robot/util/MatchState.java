1e3bda9f389e2c036788b68c8041d28913cf041d
JacobyIA <jacoby.adwin@gmail.com>
2026-03-05 20:21:14 -0500

    Begin javadocs for MatchState

--- src/main/java/frc/robot/util/MatchState.java
+++ src/main/java/frc/robot/util/MatchState.java
@@ -12,6 +12,14 @@ public class MatchState {
 
     private static Optional<Alliance> autoVictor = Optional.empty();
 
+    /**
+     * Method to determine the winner of the autonomous period, done by accessing
+     * the {@link DriverStation}'s game-specific message. This can be used, with the
+     * match time, to determine which hub is active.
+     * 
+     * @return an {@link Optional} of an {@link Alliance}, representing whether or
+     *         not we know the winner of auto and, if so, who it was.
+     */
     public static Optional<Alliance> getAutoVictor() {
         if (autoVictor.isPresent()) {
             return autoVictor;
@@ -31,7 +39,13 @@ public class MatchState {
         }
     }
 
-    public static boolean didWeWinAuto(Alliance us){
-        return us.equals(autoVictor.get());
+    /**
+     * Whether or not the
+     * 
+     * @param allianceToCheck an {@link Alliance}
+     * @return
+     */
+    public static boolean didWinAuto(Alliance allianceToCheck) {
+        return allianceToCheck.equals(autoVictor.get());
     }
 }
