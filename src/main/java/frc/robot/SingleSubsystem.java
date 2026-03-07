1c3f7fb0a600a322b3b87596b620b65a66a0c130
Sebastian Gracia <sebastian.r.gracia@gmail.com>
2026-03-07 13:18:46 -0500

    dum

--- src/main/java/frc/robot/SingleSubsystem.java
+++ src/main/java/frc/robot/SingleSubsystem.java
@@ -2,6 +2,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.util.NTable;
 
@@ -9,6 +10,7 @@ public abstract class SingleSubsystem extends TimedRobot {
     protected final CommandXboxController controller = new CommandXboxController(0);
 
     protected SingleSubsystem() {
+        NTable.root().set("scheduler", CommandScheduler.getInstance());
         init();
     }
 
@@ -26,25 +28,10 @@ public abstract class SingleSubsystem extends TimedRobot {
 
     @Override
     public void disabledPeriodic() {
-        for (LimelightSubsystem limelight : limelights) {
-            limelight.setIMUToPigeon();
-        }
     }
 
     @Override
     public void autonomousInit() {
-        for (LimelightSubsystem limelight : limelights) {
-            limelight.setIMUMode(3);
-        }
-
-        Command auto = autoChooser.getSelected();
-        if (auto == null) {
-            DriverStation.reportWarning("null auto command retrieved!", false);
-            return;
-        }
-
-        storedAuto = auto;
-        CommandScheduler.getInstance().schedule(storedAuto);
     }
 
     @Override
@@ -53,13 +40,6 @@ public abstract class SingleSubsystem extends TimedRobot {
 
     @Override
     public void teleopInit() {
-        if (storedAuto != null) {
-            storedAuto.cancel();
-        }
-
-        for (LimelightSubsystem limelight : limelights) {
-            limelight.setIMUMode(3);
-        }
     }
 
     @Override
@@ -68,7 +48,6 @@ public abstract class SingleSubsystem extends TimedRobot {
 
     @Override
     public void testInit() {
-        CommandScheduler.getInstance().cancelAll();
     }
 
     @Override
