e531bf923aabd5663da8891d42e5068f03027d79
Sebastian Gracia <sebastian.r.gracia@gmail.com>
2026-03-07 13:11:56 -0500

    fix singlesubsystem

--- src/main/java/frc/robot/SingleSubsystem.java
+++ src/main/java/frc/robot/SingleSubsystem.java
@@ -9,6 +9,7 @@ public abstract class SingleSubsystem extends TimedRobot {
     protected final CommandXboxController controller = new CommandXboxController(0);
 
     protected SingleSubsystem() {
+        init();
     }
 
     protected abstract void init();
@@ -17,5 +18,68 @@ public abstract class SingleSubsystem extends TimedRobot {
     public void robotPeriodic() {
         CommandScheduler.getInstance().run();
         NTable.updateAllSendables();
+    }
+
+    @Override
+    public void disabledInit() {
+    }
+
+    @Override
+    public void disabledPeriodic() {
+        for (LimelightSubsystem limelight : limelights) {
+            limelight.setIMUToPigeon();
+        }
+    }
+
+    @Override
+    public void autonomousInit() {
+        for (LimelightSubsystem limelight : limelights) {
+            limelight.setIMUMode(3);
+        }
+
+        Command auto = autoChooser.getSelected();
+        if (auto == null) {
+            DriverStation.reportWarning("null auto command retrieved!", false);
+            return;
+        }
+
+        storedAuto = auto;
+        CommandScheduler.getInstance().schedule(storedAuto);
+    }
+
+    @Override
+    public void autonomousPeriodic() {
+    }
+
+    @Override
+    public void teleopInit() {
+        if (storedAuto != null) {
+            storedAuto.cancel();
+        }
+
+        for (LimelightSubsystem limelight : limelights) {
+            limelight.setIMUMode(3);
+        }
+    }
+
+    @Override
+    public void teleopPeriodic() {
+    }
+
+    @Override
+    public void testInit() {
+        CommandScheduler.getInstance().cancelAll();
+    }
+
+    @Override
+    public void testPeriodic() {
+    }
+
+    @Override
+    public void simulationInit() {
+    }
+
+    @Override
+    public void simulationPeriodic() {
     }
 }
