4fa00939c8fdc59f24125704b74d836bebb3cd5c
JacobyIA <jacoby.adwin@gmail.com>
2026-03-11 19:20:56 -0400

    Set turret limit back to the safest value for now

--- src/main/java/frc/robot/constants/TurretConstants.java
+++ src/main/java/frc/robot/constants/TurretConstants.java
@@ -32,7 +32,7 @@ public class TurretConstants {
 
     // Imaginary, robot-relative hard stops for the turret; all setpoints are to be
     // within a set padding of these
-    public static final Angle REVERSE_LIMIT_BOT_REL = Rotations.of(0.48); // limit the turret would hit whild driving CW
+    public static final Angle REVERSE_LIMIT_BOT_REL = Rotations.of(0.56); // limit the turret would hit whild driving CW
     public static final Angle FORWARD_LIMIT_BOT_REL = Rotations.of(0.306); // limit the turret would hit while driving
                                                                            // CCW
     public static final Angle ZERO_OFFSET = Rotations
@@ -59,7 +59,7 @@ public class TurretConstants {
      *         the bounds.
      */
     public static Angle formatInputPosRobotRel(Angle input) {
-        return formatInputPosTurretRel(input.minus(ZERO_OFFSET));
+        return formatInputPosTurretRel(robotRelToTurretRel(input));
     }
 
     public static State formatInputStateRobotRel(State input) {
