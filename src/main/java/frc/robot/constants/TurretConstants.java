54789eccbfb96962cb39e57228c7bcc8fab4077c
JacobyIA <jacoby.adwin@gmail.com>
2026-03-12 18:53:17 -0400

    Small nudge to turret offsets

--- src/main/java/frc/robot/constants/TurretConstants.java
+++ src/main/java/frc/robot/constants/TurretConstants.java
@@ -33,7 +33,7 @@ public class TurretConstants {
     // Imaginary, robot-relative hard stops for the turret; all setpoints are to be
     // within a set padding of these
     public static final Angle REVERSE_LIMIT_BOT_REL = Rotations.of(0.56); // limit the turret would hit whild driving CW
-    public static final Angle FORWARD_LIMIT_BOT_REL = Rotations.of(0.320); // limit the turret would hit while driving
+    public static final Angle FORWARD_LIMIT_BOT_REL = Rotations.of(0.312); // limit the turret would hit while driving
                                                                            // CCW
     public static final Angle ZERO_OFFSET = Rotations
             .of((REVERSE_LIMIT_BOT_REL.in(Rotations) + FORWARD_LIMIT_BOT_REL.in(Rotations)) / 2);
