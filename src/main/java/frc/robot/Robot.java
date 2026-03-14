138f6e6eae475ed62b5428a939b73d3cc26399df
Framework laptop controlfreaks account <please.use.your.own@account.com>
2026-03-14 09:13:29 -0400

    Revert "final comp auto"
    This reverts commit 22f7c32a1360ae5d9d257c060ed70f7833a9effd.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -149,6 +149,7 @@ public class Robot extends TimedRobot {
         commandsForAuto.put("Turret track Blue hub",
                 turret.trackFieldPos(FieldConstants.alliance(FieldConstants.BLUE_HUB_CENTER)));
         NamedCommands.registerCommands(commandsForAuto);
+
         autoChooser = AutoBuilder.buildAutoChooser();
 
         SmartDashboard.putData("Choose an Auto", autoChooser);
