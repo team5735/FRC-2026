b4c80e8056196f9e04174f7576dccca961f1a958
JacobyIA <jacoby.adwin@gmail.com>
2026-03-13 20:01:39 -0400

    Final tuning of the zeroing (hello from North Shore)

--- src/main/java/frc/robot/constants/TurretConstants.java
+++ src/main/java/frc/robot/constants/TurretConstants.java
@@ -33,7 +33,7 @@ public class TurretConstants {
     // Imaginary, robot-relative hard stops for the turret; all setpoints are to be
     // within a set padding of these
     public static final Angle REVERSE_LIMIT_BOT_REL = Rotations.of(0.56); // limit the turret would hit whild driving CW
-    public static final Angle FORWARD_LIMIT_BOT_REL = Rotations.of(0.312); // limit the turret would hit while driving
+    public static final Angle FORWARD_LIMIT_BOT_REL = Rotations.of(0.324); // limit the turret would hit while driving
                                                                            // CCW
     public static final Angle ZERO_OFFSET = Rotations
             .of((REVERSE_LIMIT_BOT_REL.in(Rotations) + FORWARD_LIMIT_BOT_REL.in(Rotations)) / 2);
@@ -45,7 +45,7 @@ public class TurretConstants {
     public static final Angle TOLERANCE = Degrees.of(1);
 
     // Ideal robot-relative starting angle
-    public static final Angle START_POS_BOT_REL = Rotations.of(0.25);
+    public static final Angle START_POS_BOT_REL = Rotations.of(0);
 
     /**
      * Clamps a goal angle and to the functional range of this subsystem, converting
