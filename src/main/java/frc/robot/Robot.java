2ec5b3724bb30813ed9b0f006ee47c2d7d5b6bcd
Sebastian Gracia <sebastian.r.gracia@gmail.com>
2026-03-14 11:37:46 -0400

    formatting

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -216,44 +216,42 @@ public class Robot extends TimedRobot {
 
         // @formatter:off
         // drive to and along the arc
-        driveController.a()
-            .whileTrue(
-                new SequentialCommandGroup(
-                    Commands.runOnce(() -> this.lastDroveToArc = true),
-                    // drive to the arc, this ends when we're at the arc
-                    new PIDToPose(drivetrain,
-                        () -> targetArc.getShootingPose(
-                            drivetrain.getEstimatedPosition().getTranslation(),
-                            Rotation2d.kCW_90deg
-                        ),
-                        "drive to arc"),
-                    // if we haven't been cancelled by now, let the driver drive along the arc
-                    new DriveOnArc(drivetrain, targetArc,
-                        () -> MathUtil.applyDeadband(driveController.getLeftX(), 0.1),
-                        Rotation2d.kCW_90deg)
-                ).withName("drive to and on arc")
-            );
+        driveController.a().whileTrue(
+            new SequentialCommandGroup(
+                Commands.runOnce(() -> this.lastDroveToArc = true),
+                // drive to the arc, this ends when we're at the arc
+                new PIDToPose(drivetrain,
+                    () -> targetArc.getShootingPose(
+                        drivetrain.getEstimatedPosition().getTranslation(),
+                        Rotation2d.kCW_90deg
+                    ),
+                    "drive to arc"),
+                // if we haven't been cancelled by now, let the driver drive along the arc
+                new DriveOnArc(drivetrain, targetArc,
+                    () -> MathUtil.applyDeadband(driveController.getLeftX(), 0.1),
+                    Rotation2d.kCW_90deg)
+            ).withName("drive to and on arc")
+        );
 
         // drive to the nearest ferry shoot position
-        driveController.x()
-            .whileTrue(
-                new SequentialCommandGroup(
-                    Commands.runOnce(() -> this.lastDroveToArc = false),
-                    hood.runOnce(() -> hood.setHoodAngle(HoodConstants.HIGHEST_ANGLE_DEGREES)),
-                    // drive to the nearest shooting start position
-                    new PIDToPose(drivetrain,
-                        () -> {
-                            Pose2d estimate = drivetrain.getEstimatedPosition();
-                            Translation2d res = FieldConstants.closestFerryShootPos(estimate.getTranslation());
-                            Rotation2d offset = estimate.getRotation();
-                            if (!turret.canTurnTo(res)) {
-                                offset = offset.plus(Rotation2d.kCW_90deg);
-                            }
-                            return new Pose2d(res, offset);
-                        },
-                    "drive to shooting pos")
-                ).withName("drive to shooting pos")
-            );
+        driveController.x().whileTrue(
+            new SequentialCommandGroup(
+                Commands.runOnce(() -> this.lastDroveToArc = false),
+                hood.runOnce(() -> hood.setHoodAngle(HoodConstants.HIGHEST_ANGLE_DEGREES)),
+                // drive to the nearest shooting start position
+                new PIDToPose(drivetrain,
+                    () -> {
+                        Pose2d estimate = drivetrain.getEstimatedPosition();
+                        Translation2d res = FieldConstants.closestFerryShootPos(estimate.getTranslation());
+                        Rotation2d offset = estimate.getRotation();
+                        if (!turret.canTurnTo(res)) {
+                            offset = offset.plus(Rotation2d.kCW_90deg);
+                        }
+                        return new Pose2d(res, offset);
+                    },
+                "drive to shooting pos")
+            ).withName("drive to shooting pos")
+        );
 
 
         // set the hood angle
