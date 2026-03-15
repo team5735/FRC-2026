f67bb86a295d80da362383991056bb10708e5862
btho7793 <77469772+btho7793@users.noreply.github.com>
2026-03-15 12:05:44 -0400

    tune hood due to replacement

--- src/main/java/frc/robot/constants/HoodConstants.java
+++ src/main/java/frc/robot/constants/HoodConstants.java
@@ -5,8 +5,8 @@ public class HoodConstants {
     // and highest positions. The servo can physically be set
     // between 0 and 1. But these limit that by the physical reality
     // of the hood
-    public static final double LOWEST_SERVO_POSITION = 0.12555;
-    public static final double HIGHEST_SERVO_POSITION = 0.65;
+    public static final double LOWEST_SERVO_POSITION = 0.675;
+    public static final double HIGHEST_SERVO_POSITION = 1.165;
 
     // The range of angles (in degrees) that the physcial shooter hood can
     // move to. While tuning the hood servo, these don't change
@@ -14,8 +14,7 @@ public class HoodConstants {
     public static final double HIGHEST_ANGLE_DEGREES = 45.0;
 
     // TODO: test
-    public static final double ANGLE_AT_ARC = 8.0;
-    public static final double POS_AT_ARC = 0.575;
+    public static final double ANGLE_AT_ARC = 20.0;
 
     // 0.4 = 1.8v
     public static final double SERVO_VOLTAGE_REF0 = 0.45; // setpoint
