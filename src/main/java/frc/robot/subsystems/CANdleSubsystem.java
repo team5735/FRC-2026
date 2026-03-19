7b67642bc10e391ee07da5a88e9ccb3298511f2b
MathMan5 <Ryan.Blocker@outlook.com>
2026-03-18 20:05:10 -0400

    Added CANdle subsystem, Awaiting controller support code.

--- src/main/java/frc/robot/subsystems/CANdleSubsystem.java
+++ src/main/java/frc/robot/subsystems/CANdleSubsystem.java
@@ -10,22 +10,28 @@ import com.ctre.phoenix6.signals.StripTypeValue;
 import edu.wpi.first.wpilibj.util.Color;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
 import frc.robot.constants.Constants;
 
 public class CANdleSubsystem extends SubsystemBase {
-    private CANdle candle = new CANdle(Constants.CANDLE_ID);
+
+    private final CANdle candle = new CANdle(Constants.CANDLE_ID);
+
+    private final SolidColor colorRequest = new SolidColor(0, 399);
 
     public CANdleSubsystem() {
-        super();
-
         candle.getConfigurator().apply(
-                new CANdleConfiguration().withLED(
-                        new LEDConfigs().withStripType(StripTypeValue.GRB)));
+            new CANdleConfiguration().withLED(
+                new LEDConfigs().withStripType(StripTypeValue.GRB)
+            )
+        );
     }
 
-    SolidColor colorRequest = new SolidColor(0, 399);
-
-    public Command getSetColor(Color color) {
-        return run(() -> candle.setControl(colorRequest.withColor(new RGBWColor(color))));
+    public Command setColor(Color color) {
+        return runOnce(() -> 
+            candle.setControl(
+                colorRequest.withColor(new RGBWColor(color))
+            )
+        );
     }
 }
