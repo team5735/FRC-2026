db902a285287b98a458497d12c6dbfc1d69dc79e
Sebastian Gracia <sebastian.r.gracia@gmail.com>
2026-03-23 20:52:09 -0400

    re-enable auto-zeroing of turret

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -388,9 +388,9 @@ public class Robot extends TimedRobot {
             limelight.setIMUMode(3);
         }
 
-        // if (!turret.getZeroStatus()) {
-        // CommandScheduler.getInstance().schedule(turret.zeroSequence());
-        // }
+        if (!turret.getZeroStatus()) {
+            CommandScheduler.getInstance().schedule(turret.zeroSequence());
+        }
 
         launcher.stop();
     }
