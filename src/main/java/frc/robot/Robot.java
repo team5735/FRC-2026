a6b107865645a7f389e0a5d24e87f8da2e45e78e
btho7793 <77469772+btho7793@users.noreply.github.com>
2026-03-28 09:25:38 -0400

    Add turret auto-zero to autonomous init

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -318,9 +318,9 @@ public class Robot extends TimedRobot {
             limelight.setIMUMode(3);
         }
 
-        // if (!turret.getZeroStatus()) {
-        // CommandScheduler.getInstance().schedule(turret.zeroSequence());
-        // }
+        if (!turret.getZeroStatus()) {
+        CommandScheduler.getInstance().schedule(turret.zeroSequence());
+        }
 
         Command auto = autoChooser.getSelected();
         if (auto == null) {
